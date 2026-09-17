edab97755ea47e240c796bd954474fa48563d898
chiinsurancebrokers <info@chiinsurancebrokers.com>
2026-09-17 18:46:52 +0300

    feat: export deterministic document intelligence helpers

--- backend/app/documents/__init__.py
+++ backend/app/documents/__init__.py
@@ -2,6 +2,8 @@
 
 from .bridge import ProposalBridgeResult, apply_proposal_analysis
 from .extraction import ExtractionResult, extract_document
+from .headline import analyze_quote_headlines
+from .plan_selector import identify_selected_plan
 from .proposal_adapter import analysis_to_facts
 from .quality import assess_result_quality
 
@@ -9,7 +11,9 @@ __all__ = [
     "ExtractionResult",
     "ProposalBridgeResult",
     "analysis_to_facts",
+    "analyze_quote_headlines",
     "apply_proposal_analysis",
     "assess_result_quality",
     "extract_document",
+    "identify_selected_plan",
 ]
